cf80d0b0237f981d2467c9ef021a0df6a2658a05
suhas-koheda <sharmasuhas450@gmail.com>
2025-01-15 01:52:59 +0530

    Carousel

--- src/app/(components)/Category/Categories.tsx
+++ src/app/(components)/Category/Categories.tsx
@@ -1,13 +1,14 @@
 import CategoryCarousel from "@/app/(components)/Category/CategoryCarousel";
+import Link from "next/link";
 
 export default function Categories() {
     return (<div className="px-2">
         <div className="flex justify-between items-center px-4">
             <h1 className="text-2xl font-semibold p-2">Shop By Category</h1>
-            <h4 className="text-l font-semibold p-2 hidden md:block">Browse all categories</h4></div>
+            <Link href={"/categories"} className="text-l font-semibold p-2 hidden md:block">Browse all categories</Link></div>
         <CategoryCarousel/>
         <div className="block md:hidden p-2">
-            <h4 className="text-l font-semibold p-2 ">Browse all categories</h4>
+            <Link href={"/categories"} className="text-l font-semibold p-2 block md:hidden">Browse all categories</Link>
         </div>
     </div>)
 }
